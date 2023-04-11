63048af6d5236c5d35563aa7e2da299dce9bf144
kiran1616 <kirankumar7163@gmail.com>
2023-04-11 20:13:45 +0530

    updated script

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_docdb_cluster" "docdb" {
   master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
   master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
   skip_final_snapshot     = true
-  db_subnet_group_name    = aws_security_group.docdb.id
+  db_subnet_group_name    = aws_docdb_subnet_group.default.name
   vpc_security_group_ids  = [aws_security_group.docdb.id]
 
   tags = merge(
