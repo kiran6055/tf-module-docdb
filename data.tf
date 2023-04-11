44781301552b95cd8a915f05035e16240da856c2
kiran1616 <kirankumar7163@gmail.com>
2023-04-11 20:18:58 +0530

    updated script

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_ssm_parameter" "DB_ADMIN_USER" {
-  name = "${var.env}DB_ADMIN_USER "
+  name = "dev.docdb.DB_ADMIN_USER "
 }
 
 data "aws_ssm_parameter" "DB_ADMIN_PASS" {
-  name = "${var.env}DB_ADMIN_PASS"
+  name = "dev.docdb.DB_ADMIN_PASS"
 }
