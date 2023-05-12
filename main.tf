678cfa7398cc274587bc7ca85e90678bf9e3c433
kiran1616 <kirankumar7163@gmail.com>
2023-05-12 17:02:40 +0530

    mew

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #creating subnetgroup for DB
 resource "aws_docdb_subnet_group" "default" {
-  name       = "${var.env}-docdb-subnet-groups"
-  subnet_ids = var.subnet_ids
+  name        = "${var.env}-docdb-subnet-groups"
+  subnet_ids  = var.subnet_ids
 
   tags = merge(
     local.common_tags,
